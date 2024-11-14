061cc5c8a265d0283452d7937f15b587f8d26863
chenzihong <2295214749@qq.com>
2024-11-14 15:15:19 +0800

    build(projects): vite.config新增resolve、新增全局类型

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import process from "node:process";
 import { defineConfig, loadEnv } from "vite";
 import { setupVitePlugins } from "./build/plugins";
 import { getBuildTime } from "./build/config/time";
+import { fileURLToPath } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig((configEnv) => {
@@ -11,6 +12,12 @@ export default defineConfig((configEnv) => {
   ) as unknown as Env.ImportMeta;
   const buildTime = getBuildTime();
   return {
+    resolve: {
+      alias: {
+        "~": fileURLToPath(new URL("./", import.meta.url)),
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
     css: {
       preprocessorOptions: {
         scss: {
