9fe7418c49e411553c46b2b2ada67abf7f12f563
chenzihong <2295214749@qq.com>
2024-11-18 18:33:41 +0800

    feat-wip(packages): 新增hooks

--- src/layouts/blank-layout/index.vue
+++ src/layouts/blank-layout/index.vue
@@ -3,10 +3,16 @@
   <SvgIcon local-icon="activity" class="text-30px text-primary"></SvgIcon>
   <icon-local-activity class="text-30px text-primary" />
   <h3 class="text-28px text-primary font-500 lt-sm:text-22px">{{ $t('system.title') }}</h3>
+  {{ a.num }}
+  <n-button @click="a.num++">点击</n-button>
 </template>
 
 <script setup lang="ts">
   import { $t } from '@/locales';
+  import { ref } from 'vue';
+  const a = ref({
+    num: 1,
+  });
 </script>
 
 <style scoped lang="scss"></style>
