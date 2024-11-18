c28cfa1667cf2d56a8bcf9386ca7f6fd6008155a
chenzihong <2295214749@qq.com>
2024-11-18 15:17:53 +0800

    feat(projects): 新增i18n

--- src/layouts/blank-layout/index.vue
+++ src/layouts/blank-layout/index.vue
@@ -2,8 +2,11 @@
   <div class="text-red-800">123</div>
   <SvgIcon local-icon="activity" class="text-30px text-primary"></SvgIcon>
   <icon-local-activity class="text-30px text-primary" />
+  <h3 class="text-28px text-primary font-500 lt-sm:text-22px">{{ $t('system.title') }}</h3>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { $t } from '@/locales';
+</script>
 
 <style scoped lang="scss"></style>
